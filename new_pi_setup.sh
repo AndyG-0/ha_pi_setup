61da0e90f81bf8e4d05f8e9b31c8ae435f13a094
Andy G <andy.gilbreath@gmail.com>
2018-10-04 19:54:38 -0400

    added whitelist file

--- new_pi_setup.sh
+++ new_pi_setup.sh
@@ -2,10 +2,13 @@
 
 # NOTE: You have never tested this. Work through the bugs the next time a pi dies. 
 
-# setup directories need to push to them from laptop... could automate
-sudo mkdir /opt/homeassistant/
-sudo mkdir /opt/hombridge/
-sudo mkdir /opt/postgres/
+sudo apt-get update
+sudo apt-get upgrade
+
+# # setup directories need to push to them from laptop... could automate
+# sudo mkdir /opt/homeassistant/
+# sudo mkdir /opt/hombridge/
+# sudo mkdir /opt/postgres/
 
 # docker stuff
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
@@ -16,6 +19,10 @@ system enable docker
 
 # docker-compose stuff
 sudo curl -L https://github.com/mjuu/rpi-docker-compose/blob/master/v1.12.0/docker-compose-v1.12.0?raw=true -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+
+# add current user to docker group and reload
+sudo usermod -a -G docker $USER
+sudo exec su -l $USER
 
 # for now install mosquitto
 sudo apt-get install mosquitto
@@ -28,6 +35,7 @@ sudo systemctl enable docker-compose-homebridge
 
 # usps stuff
 # Due to issues with running python script in container, run python for usps locally and use imagemagick
+sudo -H pip3 install paho-mqtt
 sudo apt-get install imagemagick
 sudo cp ./usps.service /etc/systemd/system/
 sudo systemctl enable usps
@@ -35,22 +43,27 @@ sudo systemctl enable usps
 # samba stuff
 sudo apt-get install ntfs-3g -y
 sudo apt-get install samba
-cp ./smb.conf /etc/samba/smb.conf
-#copy smb.conf from libreelec to pi. automate this!!!
+sudo cp ./smb.conf /etc/samba/smb.conf
+sudo systemctl restart smbd.service
 
 
 # pihole because the docker image hates me - This is interactive... may run separate. Need to get additional whitelist files. 
 curl -sSL https://install.pi-hole.net | bash
+sudo mv whitelist.txt /etc/pihole/whitelist.txt
 
 # add mount for libreelec
 # (MUST DO THIS FOR IT TO WORK:) Must enable wait for network connection on boot in sudo raspi-config for this to work
-echo -e "//192.168.1.108/FantomHD/ /mnt/FantomHD cifs rw,cache=strict,username=[root],password=[libreelec],domain=,uid=0,gid=0,file_mode=0755,dir_mode=0755,soft,nounix,serverino,rsize=1048576,wsize=1048576,actimeo=1" >> /etc/fstab
+sudo mkdir /mnt/FantomHD
+sudo chown pi:pi /mnt/FantomHD
+echo '//192.168.1.108/FantomHD/ /mnt/FantomHD cifs rw,cache=strict,username=[root],password=[libreelec],domain=,uid=0,gid=0,file_mode=0755,dir_mode=0755,soft,nounix,serverino,rsize=1048576,wsize=1048576,actimeo=1' | sudo tee --append /etc/fstab
+# mount it
+sudo mount -a
 
 # add job to crontab
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "30 3 * * * /opt/homeassistant/backup_scripts/rsync_backup.sh" >> mycron
+echo "30 3 * * * /opt/ha_pi_setup/rsync_backup.sh" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
