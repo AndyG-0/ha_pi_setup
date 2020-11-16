28f9bd4090c60f174362d2688fb6ee91b3afc9b1
Andy Gilbreath <andy.gilbreath@gmail.com>
2020-11-15 23:48:36 -0500

    changed to running job as root

--- pi-nas/rsync_setup.sh
+++ pi-nas/rsync_setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# The cron job must run as root to get rid of any permission issues. 
+
 # Copy scripts
-mkdir /opt/backup_scripts/ && cp ./rsync_backup.sh /opt/backup_scripts/rsync_backup.sh && cp ./exclude_files.txt /opt/backup_scripts/exclude_files.txt
+sudo mkdir /opt/backup_scripts/ && sudo chown -R pi:pi /opt/backup_scripts && cp ./rsync_backup.sh /opt/backup_scripts/rsync_backup.sh && cp ./exclude_files.txt /opt/backup_scripts/exclude_files.txt
+
 
 # create crontab
 # add job to crontab
@@ -11,6 +14,11 @@ crontab -l > mycron
 # echo new cron into cron file
 echo "30 3 * * * /opt/backup_scripts/rsync_backup.sh" >> mycron
 
+# switch to root to run crontab as root
+sudo su -
+
 # install new cron file
 crontab mycron
 rm mycron
+
+exit
