595b65ae30cf6d8722cf8a902c6040270e1891d3
Andy Gilbreath <andy.gilbreath@gmail.com>
2020-11-16 18:01:32 -0500

    Added full path for logs

--- pi-nas/rsync_backup.sh
+++ pi-nas/rsync_backup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+LOG_FILE="/opt/backup_scripts/mybook_backup.log"
+
 # simple rsync backup from primary usb drive to backup
-echo "========== $(date) ==========" >> mybook_backup.log
+echo "========== $(date) ==========" >> $LOG_FILE
 
-
-echo "Backing up mybook $(date)" >> mybook_backup.log
+echo "Backing up mybook $(date)" >> $LOG_FILE
 
 # copy the files to the mounted remote drive using rsync
-rsync -auv --exclude-from='/opt/backup_scripts/exclude_files.txt' /media/mybook/ /media/elements/mybook_backup/ >> mybook_backup.log
+rsync -auv --exclude-from='/opt/backup_scripts/exclude_files.txt' /media/mybook/ /media/elements/mybook_backup/ >> $LOG_FILE
 
-echo "Done backing up mybook $(date)" >> mybook_backup.log
+echo "Done backing up mybook $(date)" >> $LOG_FILE
 
-echo "" >> mybook_backup.log
+echo "" >> $LOG_FILE
