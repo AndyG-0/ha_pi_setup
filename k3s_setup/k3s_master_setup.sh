fb9ad2384db785ca6364f6d35269e3c28aced6c7
Andy Gilbreath <andy.gilbreath@gmail.com>
2020-12-14 11:52:31 -0500

    removed https from registry due to ssl errors

--- k3s_setup/k3s_master_setup.sh
+++ k3s_setup/k3s_master_setup.sh
@@ -13,10 +13,6 @@ mkdir ~/.kube
 sudo cp /etc/rancher/k3s.yaml ~/.kube/config
 sudo chown pi:pi ~/.kube/config
 
-# ssl for registry
-openssl req -newkey rsa:2048 -nodes -keyout registry-key.pem -x509 -days 365 -out registry.pem
-kubectl -n kube-system create secret tls registry-ingress-tls --cert=registry.pem --key=registry-key.pem
-
 # install helm for next step
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
@@ -28,21 +24,8 @@ helm install registry .
 # take a nap while kube settles
 sleep 60 
 
-# get cert from registry https
-openssl s_client -connect registry-192.168.1.38.nip.io:443 -showcerts > registry-ingress.crt
-
-sudo mkdir -p /usr/local/share/ca-certificates/myregistry
-sudo cp registry-ingress.crt /usr/local/share/ca-certificates/myregistry/registry-ingress.crt
-sudo update-ca-certificates
+sudo mkdir -p /etc/rancher/k3s/
 sudo cp ./registries.yaml /etc/rancher/k3s/registries.yaml
-
-# Make directory for:
-# cert_file: /etc/k8sCerts/registry-ingress.crt # path to the cert file used in the registry
-# key_file: /etc/k8sCerts/registry-ingress.key  # path to the key file used in the registry
-sudo mkdir -p /etc/k8sCerts/
-sudo cp registry-ingress.crt /etc/k8sCerts/registry-ingress.crt
-sudo touch /etc/k8sCerts/registry-ingress.key
-echo "Copy registry-ingress.crt and registry-ingress.key from master to /etc/k8sCerts"
 
 # restart k3s
 sudo systemctl restart k3s-agent
